15b93873cf4a65c182f1a8837782d1071813687b
NivedDwivedi <64701780+NivedDwivedi@users.noreply.github.com>
2020-10-31 11:27:13 +0530

    Add files via upload

--- 2D BreakOut Game/script.js
+++ 2D BreakOut Game/script.js
@@ -5,6 +5,25 @@ const ctx = canvas.getContext("2d");
 // canvasW = canvas.width;
 // canvasH = canvas.height;
 
+if("orientation" in screen) {
+  if(document.documentElement.requestFullscreen) 
+  {
+    document.documentElement.requestFullscreen();
+  } 
+  else if( document.documentElement.mozRequestFullscreen ) 
+  {
+    document.documentElement.mozRequestFullscreen();
+  } 
+  else if( document.documentElement.webkitRequestFullscreen ) {
+    document.documentElement.webkitRequestFullscreen();
+  } 
+  else {
+    document.documentElement.msRequestFullscreen();
+  } 
+
+  screen.orientation.lock('landscape-primary');
+}
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // canvas.width=600;
